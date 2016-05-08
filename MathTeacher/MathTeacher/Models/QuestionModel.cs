ba2eb66b8285658d89a4e19b3f37fbad40fc413c
BK_5000 <brian.kidd.one@gmail.com>
2016-05-07 20:16:02 -0700

    Models for MathGame

---
+++ MathTeacher/MathTeacher/Models/QuestionModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elena_Test.Models
+{
+    public class QuestionModel
+    {
+        public int QuestionID { get; set;  }
+        public int Grade { get; set; }
+    }
+}
